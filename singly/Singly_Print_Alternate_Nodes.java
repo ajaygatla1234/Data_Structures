cb28e1909303fe7dbb40f5fe8445f993ae734be0
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2023-11-07 17:40:12 +0530

    Add files via upload

---
+++ singly/Singly_Print_Alternate_Nodes.java
@@ -0,0 +1,84 @@
+package com.linkedlist.singly;
+
+public class Singly_Print_Alternate_Nodes {
+
+	public static Node printAlternateNodes1(Node head) {
+
+		if(head==null||head.nextNode==null) {
+			return head;
+		}
+
+		Node currentNode = head;
+
+		while(head!=null&&head.nextNode!=null) {
+
+			head.nextNode = head.nextNode.nextNode;
+			head = head.nextNode;
+		}
+
+		return currentNode;
+
+	}
+
+	public static void printAlternateNodes2(Node head) {
+
+		int count = 0; 
+		while (head != null)
+		{ 
+
+			if (count % 2 == 0) {                     // when count is even print the nodes
+				System.out.print(head.data+" ");
+			}
+			count++;                                  // count the nodes 
+			head = head.nextNode;                     // move on the next node. 
+
+		} 
+	}
+
+
+	public static void display(Node headNode) {
+		Node currentNode = headNode;
+		while(currentNode!=null) {
+			System.out.print(currentNode.data+" ");
+			currentNode = currentNode.nextNode;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		//Adding the data to the nodes
+		Node head = new Node(10);
+
+		Node secondNode = new Node(20);
+
+		Node thirdNode = new Node(30);
+
+		Node fourthNode = new Node(40);
+
+		Node fifthNode = new Node(50);
+
+		//Adding References
+		head.nextNode = secondNode;
+
+		secondNode.nextNode = thirdNode;
+
+		thirdNode.nextNode = fourthNode;
+
+		fourthNode.nextNode = fifthNode;
+
+		fifthNode.nextNode = null;
+
+		System.out.println("Given Input :");
+		display(head);
+
+		System.out.println("\n\noutput :");
+		Node newHead = printAlternateNodes1(head);
+		display(newHead);
+
+		System.out.println("\n\noutput :");
+		printAlternateNodes2(head);
+
+	}
+
+
+}

1e5d6ae442f459232861d529e200e44a6a7a0427
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2024-04-29 16:11:58 +0530

    Add files via upload

---
+++ Doubly/NodeDoubly.java
@@ -0,0 +1,15 @@
+package com.gl.linkedlist;
+
+public class NodeDoubly {
+	
+	int data;
+	NodeDoubly previousNode;
+	NodeDoubly nextNode;
+	
+	public NodeDoubly(int data) {
+		this.data = data;
+	}
+	
+	
+
+}

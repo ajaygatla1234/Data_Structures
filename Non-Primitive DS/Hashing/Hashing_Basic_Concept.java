406e8bcfbfbc58f4ea5783a2cffd2f6077f517d6
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2024-07-20 17:57:38 +0530

    Update Hashing_Basic_Concept.java

--- Non-Primitive DS/Hashing/Hashing_Basic_Concept.java
+++ Non-Primitive DS/Hashing/Hashing_Basic_Concept.java
@@ -17,5 +17,18 @@ public class Hashing_Basic_Concept{
 		
 		System.out.println(Arrays.toString(hashTable));
 		System.out.println(hashTable[8]);
-	}	
+	}
+
+	/*
+ 
+	Is hashing linear or non linear data structures...?
+	
+	Hashing can be implemented in both linear and non-linear data structures, depending on the specific implementation and use case.
+		
+	Linear Implementation: When using open addressing techniques like linear probing or quadratic probing, hashing can be considered linear. These methods involve searching for an empty slot in a sequential manner when a collision occurs1.
+	Non-Linear Implementation: When using closed addressing techniques like chaining, where each slot in the hash table points to a linked list or another data structure, hashing is considered non-linear12.
+		
+  	So, hashing itself is a technique that can be adapted to both linear and non-linear data structures.
+   
+ 	*/
 }

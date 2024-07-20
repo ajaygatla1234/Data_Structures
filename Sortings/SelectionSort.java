8ac2387e928a32507c588776ada3056947bee52b
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2024-07-20 21:55:18 +0530

    Update SelectionSort.java

--- Sortings/SelectionSort.java
+++ Sortings/SelectionSort.java
@@ -26,7 +26,7 @@ public class SelectionSort {
 	/*
 	 * Algorithm Performance Analysis
 	 * Time Complexity
-	 * worst, Average & Best Case - O(n^2)
+	 * worst, Average & Best Case - O(n^2) (even in sorted array also, both for loops gets executed to find the smallest element)
 	 * Space Complexity - O(1) - Constant space complexity (inplace)
 	 * Not-Stable (in this particular code, we are not giving any consideration of the relative order of elements while swapping, No guarantee that array remains stable) (if we want we can make it stable by taking special care Programitically with extra steps)
   	 * Go through this link to understand why it is not stable : https://youtu.be/19YOpMMtLx8?feature=shared
